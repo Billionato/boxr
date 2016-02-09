4468f8a8435700023c7a4712486d48680cc89730
Theodore A. Ochieng <royalato@gmail.com>
2016-02-09 23:33:19 +0000

    Configured Mailer for Production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,4 +86,20 @@ Rails.application.configure do
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
+  
+  # Mailer
+  config.action_mailer.default_url_options = { host: 'https://ancient-dusk-7174.herokuapp.com' }
+  
+  # Setup SMTP via Gmail Account
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: 'https://ancient-dusk-7174.herokuapp.com',
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
 end
