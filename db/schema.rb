b34313f628fa61ce9246045cbaa23c2e5bba01cf
Theodore A. Ochieng <royalato@gmail.com>
2016-02-09 22:44:14 +0000

    Modified Devise setup to include Confirmable
    This allows for confrimation emails to be sent to users when they sign up for the service. A user has to click through the confirmation link in the email to finish their account creation.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160112010257) do
+ActiveRecord::Schema.define(version: 20160209223522) do
 
   create_table "boxes", force: :cascade do |t|
     t.string   "name"
@@ -61,8 +61,12 @@ ActiveRecord::Schema.define(version: 20160112010257) do
     t.string   "last_name"
     t.string   "provider"
     t.string   "uid"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
